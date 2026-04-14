a54d228ee94b1ac6ac713d7ea96710761f233375
matamata95 <116118442+matamata95@users.noreply.github.com>
2026-04-14 14:54:13 +0200

    Update run.py
    Added agent checkpoints and saving agents

--- run.py
+++ run.py
@@ -1,20 +1,38 @@
 from logger import export_statistics_logging
 from tensorforce.environments import Environment
 from tensorforce.execution import Runner
+from tensorforce.agents import Agent
+import tensorflow as tf
+from datetime import datetime
+import os
+
+# date_time = datetime.now().strftime("%Y_%m_%d_%H-%M-%S")
+AGENT_SAVE_PATH = os.path.join('agents', 'ppo1')
 
 
 # tf.set_random_seed(10)
+os.makedirs(AGENT_SAVE_PATH, exist_ok=True)
 
-timesteps = 10 ** 2  # Set time steps per episode
-episodes = 2 ** 2  # Set number of episodes
+timesteps = 10 ** 3  # Set time steps per episode
+episodes = 10 ** 3  # Set number of episodes
 
+# Define environment
 environment_production = Environment.create(
     environment='production.envs.ProductionEnv',
-    max_episode_timesteps=timesteps
+    max_episode_timesteps=timesteps,
 )
 
 # Tensorforce runner
-runner = Runner(agent='config/ppo1.json',
+agent = Agent.create(
+    agent='config/ppo2.json',
+    environment=environment_production,
+    saver={
+        'directory': os.path.join(AGENT_SAVE_PATH, 'model-checkpoint'),
+        'frequency': 10,
+        'max-checkpoints': 5
+    }
+)
+runner = Runner(agent=agent,
                 environment=environment_production)
 environment_production.agents = runner.agent
 
@@ -25,3 +43,5 @@ environment_production.environment.statistics.update({'time_end': environment_pr
 export_statistics_logging(statistics=environment_production.environment.statistics,
                           parameters=environment_production.environment.parameters,
                           resources=environment_production.environment.resources)
+# # Save agent
+agent.save(directory=AGENT_SAVE_PATH, format='tensorflow')
